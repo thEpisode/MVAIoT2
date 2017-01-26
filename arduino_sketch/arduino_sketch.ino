ab71425becaab73891bf93d36b03efb36ebec553
Cami Rodriguez <camiepisode@outlook.com>
2017-01-26 08:30:14 -0500

    added arduino buzzer

---
+++ arduino_sketch/arduino_sketch.ino
@@ -0,0 +1,38 @@
+int buzzer = 8;
+String str;
+bool beep;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // send an intro:
+  Serial.println("Hi, please send a beep 1| 1000");
+  Serial.println();
+  
+  pinMode(buzzer, OUTPUT);
+  beep = true;
+}
+
+void loop() {
+  if (Serial.available() > 0){
+
+    String first  = Serial.readStringUntil('|');
+    Serial.read();
+    String second  = Serial.readStringUntil('\0');
+    
+    /// Parse strings to int
+    int beep = first.toInt();
+    int duration = second.toInt();
+    
+    Serial.print(duration);
+    if(beep == 1){
+      tone(buzzer, 1000, duration);
+      delay (duration); 
+      noTone(8);
+    }
+  }
+  
+}
